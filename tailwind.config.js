84fbf4923a61a2b579282906d8fc0cc7270d97db
shahedul-alam <shaheedalamcontact@gmail.com>
2024-08-03 01:35:32 +0600

    header section added along with navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'header-gradient': "linear-gradient(180deg, rgb(7, 2, 17), rgba(7, 2, 17, 0) 100%), url('/images/others/hero-banner.jpg')",
+      },
+      fontFamily: {
+        'poppins': ["Poppins", "sans-serif"],
+        'inter': ["Inter", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
